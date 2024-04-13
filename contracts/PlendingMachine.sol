7338674544da9e2d9891623d714b7899093c210b
May Tusek <tusek.may@gmail.com>
2024-04-13 15:30:58 -0700

    Makes into generic template.

--- contracts/PlendingMachine.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.9;
-
-contract PlendingMachine {
-    mapping(address => uint) private _balances;
-
-    function checkBalance(address userAddress) public view returns (uint) {
-        return _balances[userAddress];
-    }
-}
